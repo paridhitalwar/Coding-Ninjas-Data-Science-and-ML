f6e618c42e64d15a4e49eda4da3220bada665e2b
Paridhi Talwar <88367084+paridhitalwar@users.noreply.github.com>
2022-06-14 15:23:18 +0530

    Create Trailing zeroes in n!.py

---
+++ More_on_Loops/Trailing zeroes in n!.py
@@ -0,0 +1,15 @@
+def findTrailingZeros(n):
+      
+    
+    count = 0
+  
+    i = 5
+    while (n / i>= 1):
+        count += int(n / i)
+        i *= 5
+  
+    return int(count)
+  
+
+n = int(input())
+print(findTrailingZeros(n))
