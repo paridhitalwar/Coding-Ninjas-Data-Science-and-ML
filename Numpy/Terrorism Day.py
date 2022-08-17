e2abd6bd236a227ad40b8e3477fd3c7d05965195
Paridhi Talwar <88367084+paridhitalwar@users.noreply.github.com>
2022-08-17 17:41:39 +0530

    Update Terrorism Day.py

--- Numpy/Terrorism Day.py
+++ Numpy/Terrorism Day.py
@@ -1,6 +1,7 @@
 # Given file "terrorismData.csv"
 # It is an open-source database including information on terrorist attacks around the world from 1970 through 2017. This dataset includes systematic data on domestic as well as international terrorist incidents that have occurred during this time period
-
+# Find the number of attack held between day 10 and day 20?(ignoring the year and month)(including both day)
+# Print count of NumberOFAttack as integer value.
 
 import numpy as np
 import csv
