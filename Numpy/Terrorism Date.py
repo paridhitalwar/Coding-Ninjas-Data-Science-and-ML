8340e1acac4250a3c9ead540ed9224108a8a2476
Paridhi Talwar <88367084+paridhitalwar@users.noreply.github.com>
2022-08-17 17:42:25 +0530

    Update Terrorism Date.py

--- Numpy/Terrorism Date.py
+++ Numpy/Terrorism Date.py
@@ -1,4 +1,8 @@
 # It is an open-source database including information on terrorist attacks around the world from 1970 through 2017. This dataset includes systematic data on domestic as well as international terrorist incidents that have occurred during this time period
+# Find the number of attack held between 1 Jan 2010 and 31 Jan 2010?(including both date).
+# Note Ignore the case where day is 0
+# Print count of NumberOFAttack as integer value.
+
 import numpy as np
 import csv
 
